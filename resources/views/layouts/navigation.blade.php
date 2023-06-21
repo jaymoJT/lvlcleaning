89b27e84cc62fdb0b6050266edd4c2e0122fa0f2
jaymoJT <jitwebdesigns@gmail.com>
2023-06-21 11:11:23 +0300

    Styled the homepage more with icons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,8 +10,8 @@
       </button>
       <div class="offcanvas offcanvas-end " tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbar2Label">LVL Cleaning</h5>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <img src="{{asset('images/lvl-logo.png')}}" style="max-width: 150px" />
+          <button type="button" class="btn-close btn-close-dark" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -27,18 +27,8 @@
 
  
     <div class="color-stripe purple-gradient ">
-        <div class="d-flex align-items-center justify-content-center " >
-            <div class="card" style="margin-top:-8%;">
-                <div class="card-body">
-                    <h5>Cleaning Estimate</h5>
-                    @include('includes.estimate') 
-                </div>
-               
-            </div>
-        </div>
-       
         <div class="container wrapper">
-            <div class="row" style="margin-bottom:4%; margin-top: 4%;">
+            <div class="row" style="margin-bottom:6%; margin-top: 4%;">
                 <div class="col-md-3 column-separator">
                     <strong>Weekly (Most Popular)</strong><br>
                     <p>
@@ -66,6 +56,15 @@
         </div>
     </div>
     <div class="why-section">
+        <div class="d-flex align-items-center justify-content-center " >
+            <div class="card" style="margin-top:-6%; box-shadow: 0px 4px 15px -3px rgba(2, 3, 73, 0.411) !important; ">
+                <div class="card-body">
+                    <h5>Cleaning Estimate</h5>
+                    @include('includes.estimate') 
+                </div>
+               
+            </div>
+        </div>
         <div class="container wrapper">
             <div class="row">
                 <div class="col-md-7">
@@ -75,7 +74,7 @@
                 </div>
                 <div class="col-md-5">
                     <div class="wrapper">
-                        <h6><strong> About lvl Cleaning</strong> </h6>
+                        <h6 style="color:#E537C2 !important;"><strong> About lvl Cleaning</strong> </h6>
                     <h1>You ofcourse need us.</h1>
                     <p>Our cleaning services are executed by a highly professional team with extensive skills and expertise. Our services will help you;</p>
                     <p><span class="material-symbols-outlined" style="color:#E537C2; font-weight:bold;">done</span> <strong>Save your time for other activities.</strong></p>
@@ -102,131 +101,62 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5>
-                                        <span class="material-symbols-outlined" style="font-size:200% !important; color:#E537C2; ">in_home_mode</span><br>
-                                        Home Cleaning
-                                    </h5>
-                                    <p>Spend time with your family as we clean your house, whether on regular basis or on a one time schedule.</p>
-                                </div>
-                            </div>
+                <div class="col-md-3">
+                    <a class="card1" href="#">
+                        <h5>Home Cleaning</h5>
+                        <p class="small">Spend time with your family as we clean your house, whether on regular basis or on a one time schedule.</p>
+                        <div class="go-corner" href="#">
+                          <div class="go-arrow">
+                            <span class="material-symbols-outlined" style="font-size:200% !important; color:#fff; ">in_home_mode</span><br>
+                          </div>
                         </div>
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5>
-                                        <span class="material-symbols-outlined" style="font-size:200% !important; color:#E537C2;">print_connect</span><br>
-                                        Commercial Cleanng</h5>
-                                        <p>We will clean your workstation, windows, store shelves, carpets and anything in your workspace.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5>
-                                        <span class="material-symbols-outlined" style="font-size:200% !important; color:#E537C2;">factory</span><br>
-                                        Industrial Cleaning</h5>
-                                        <p>Our team is skillrd in cleaning factories, plants and industries, letting your staff stay productive.</p>
-                                </div>
-                            </div>
+                <div class="col-md-3">
+                    <a class="card1" href="#">
+                        <h5>Commercial Cleaning</h5>
+                        <p class="small">We will clean your workstation, windows, store shelves, carpets and anything in your workspace; your shop or office.</p>
+                        <div class="go-corner" href="#">
+                          <div class="go-arrow">
+                            <span class="material-symbols-outlined" style="font-size:200% !important; color:#fff; ">print_connect</span><br>
+                          </div>
                         </div>
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5>
-                                        <span class="material-symbols-outlined" style="font-size:200% !important; color:#E537C2;">celebration</span><br>
-                                        After-party Cleaning</h5>
-                                        <p>People leave after a party, we come with the fresh energy to get your space to it's initial clean state.</p>
-                                </div>
-                            </div>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a class="card1" href="#">
+                        <h5>Industrial Cleaning</h5>
+                        <p class="small">Our team is skilled in cleaning factories, plants and industries, letting your staff stay productive in important roles.</p>
+                        <div class="go-corner" href="#">
+                          <div class="go-arrow">
+                            <span class="material-symbols-outlined" style="font-size:200% !important; color:#fff; ">factory</span><br>
+                          </div>
                         </div>
-                    </div>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a class="card1" href="#">
+                        <h5>After-party Cleaning</h5>
+                        <p class="small">People leave after a party, we come with the fresh energy to get your space to it's initial clean state.</p>
+                        <div class="go-corner" href="#">
+                          <div class="go-arrow">
+                            <span class="material-symbols-outlined" style="font-size:200% !important; color:#fff; ">celebration</span><br>
+                          </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 
-    {{-- <div class="home-booking">
-        <div class="container wrapper">
-            <h3 class="home-booking-text">Book a cleaning schedule</h3>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                           <form>
-                                <div class="form-group">
-                                    <label>Email Address</label>
-                                    <input type="email" class="form-control" name="email" required="require" />
-                                </div>
-                                <div class="form-group">
-                                    <label>Phone</label>
-                                    <input type="tel" class="form-control" name="phone" required="require" />
-                                </div>
-                                <div class="form-group">
-                                    <label>City</label>
-                                    <input type="text" class="form-control" name="city" required="require" />
-                                </div>
-                                <div class="form-group">
-                                    <label>Date</label>
-                                    <input type="date" class="form-control" name="date" required="require" />
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label>From</label>
-                                            <input type="time" class="form-control" name="from" required="require" />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label>To</label>
-                                            <input type="time" class="form-control" name="to" required="require" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Description</label>
-                                    <textarea class="form-control" name="description" required="require"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary">Submit</button>
-                                </div>
-                           </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 text-center py-5 ">
-                    <p class="home-booking-text">
-                        To initiate the booking process, simply send us a request, and you can expect a response from us within 12 hours. Our aim is to understand your specific requirements and ensure the best possible service for you. In the event that our team is unavailable during your preferred booking time, we are more than willing to engage in a discussion with you to find a suitable alternative that aligns with your schedule. Our priority is to accommodate your needs and deliver the highest level of service possible.
-                    </p>
-                    <div class="row">
-                        <div class="col-6">
-                            <button class="btn btn-success">Whatsapp</button>
-                        </div>
-                        <div class="col-6">
-                            <button class="btn btn-primary">Phone Call</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+    
 
-    <div class="customer-centered">
+    <div class="customer-care purple-gradient">
         <div class="container wrapper">
-            <h1>Customer-centered Service</h1>
+            <h1 style="color:#fff !important;">Customer-centered Service</h1>
             <div class="row">
                 <div class="col-md-6">
-                    <div class="wrapper text-center py-5">
+                    <div class="wrapper ">
                         <p>
                             Our cleaning services are executed by a highly professional team with extensive skills and expertise. Each member of our team undergoes thorough background checks to ensure reliability and trustworthiness. We take pride in our team's efficiency and their ability to promptly complete tasks. Our goal is to deliver a service that exceeds your expectations, leaving you with a positive and satisfying experience.
                         </p>
@@ -237,24 +167,24 @@
                 </div>
                 
             </div>
-        </div>
-    </div>
-
-    <div class="customer-care purple-gradient">
-        <div class="container wrapper">
-            <h1 style="color: #fff !important;">Served well?</h1>
-            <p class="wrapper">
-                Here is our customer care center. Please rate our performance on how we served you. Feel free to let us know immediately if you happen to be unhappy with our work at your place or if you have any particular requirements so that we can fulfill them tomorrow.
-            </p>
-            <div class="row">
-                <div class="col-6">
-                    <button class="btn btn-success">Whatsapp</button>
-                </div>
-                <div class="col-6">
-                    <button class="btn btn-primary">Phone Call</button>
+            <div class="">
+                <h4 style="color: #fff !important;">Served well?</h4>
+                <p>
+                    Here is our customer care center. Please rate our performance on how we served you. Feel free to let us know immediately if you happen to be unhappy with our work at your place or if you have any particular requirements so that we can fulfill them tomorrow.
+                </p>
+                <div class="">
+                    <div class="col-6">
+                      <button class="btn btn-success">Whatsapp</button>
+                      <button class="btn btn-primary">Phone Call</button>
+                    </div>
+                  
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="">
+        
     </div>
     <div class="why-us">
         <div class="container wrapper">
@@ -264,31 +194,47 @@
                     When you engage with LVL's, we genuinely take the time to hear about your requirements and expectations up front so that we can develop a commercial cleaning strategy that works for you. We still want to hear from you after that plan is in place.
                 </p>
             </div>
-
             <div class="row">
-                <div class="col-md-3">
-                    <strong>Trained Staff</strong>
-                    <p>
-                        You never have to be concerned about the quality of the work because every one of our maids has had their backgrounds checked, trained, and evaluated.
-                    </p>
+                <div class="col-md-6 wrapper">
+                    <img src="{{asset('images/cleaning-staff.jpeg')}} " class=" img-fluid" style="border-radius: 20px !important;" />
                 </div>
-                <div class="col-md-3">
-                    <strong>Transparent</strong>
-                    <p>
-                        We employ a clear pricing strategy and provide the price of our services prior to starting the work. No additional fees apply; supplies are included in the price.
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <strong>Customized Services</strong>
-                    <p>
-                        We are adaptable and reasonably priced, and we have tailored our services to meet all needs, from little to enormous places.
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <strong>Easy Online Booking</strong>
-                    <p>
-                        Booking cleaning services for an apartment, workplace, or home has never been simpler than with us.
-                    </p>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card features">
+                                <div class="card-body">
+                                    <strong>Trained Staff</strong>
+                                        <p>Every one of our maids is skilled and their backgrounds checked, trained, and evaluated.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card features">
+                                <div class="card-body">
+                                    <strong>Transparent</strong>
+                                    <p>Clear pricing from begining. No secret additional fees; supplies are included in the price.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card features">
+                                <div class="card-body">
+                                    <strong>Customized Services</strong>
+                                    <p>We have tailored our services to meet all needs, from little to enormous places and budgets.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card features">
+                                <div class="card-body">
+                                    <strong>Easy Online Booking</strong>
+                                    <p>It takes a few minutes to mark us into your calendar from your favourite device and anywhere.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
